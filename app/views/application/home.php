e45ad3badbce4897e9bd0243496749b132296b5a
stephanemontoro <stephane.montoro@hotmail.com>
2021-08-15 20:54:13 +0200

    add my information to home pages

--- app/views/application/home.php
+++ app/views/application/home.php
@@ -4,7 +4,28 @@
   <div>
     <h1>Mon premier blog en PhP!</h1>
     
-    <p>Bonjour Je m'appelle Stéphane</p>
+    <h3>Montoro Stéphane</h3>
+    
+    <img src="app/assets/images/pp.jpg" alt="Photo de Profil" style="width: 350px; height: 400px;">
+
+    <p>
+      Bonjour Je suis un ancien Boulanger/Pâtissier 
+      reconvertis en Développeur web depuis octobre 2019 !
+    </p>
+
+    <a href="app/assets/pdf/cv.pdf" target="blank">Voici mon C.V</a>
+
+    <p>le formulaire de contact est à venir !</p>
+    <?php // require('app/views/forms/contact_form.php'); ?>
+
+    <div class="social_link">
+      <h3>Mes Réseaux sociaux</h3>
+      <ul>
+        <li><a href="https://twitter.com/MontoroStephane" target="blank">Mon Twitter</a></li>
+        <li><a href="https://github.com/Amael7" target="blank">Mon GitHub</a></li>
+        <li><a href="https://www.linkedin.com/in/stephane-montoro/" target="blank">Mon LinkedIn</a></li>
+      </ul>
+    </div>
   </div>
 <?php $content = ob_get_clean(); ?>
 
