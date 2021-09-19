6a13b642d2fb682259cef6473bcac116fad0a221
stephanemontoro <stephane.montoro@hotmail.com>
2021-09-19 20:16:59 +0200

    add function to comment manager

--- src/Manager/CommentManager.php
+++ src/Manager/CommentManager.php
@@ -4,4 +4,7 @@ namespace App\Manager;
 
 class CommentManager extends AppManager {
 
+  public static function getCommentByAuthor($authorId, $className) {
+    return parent::dbConnect()->query('SELECT * from ' . parent::getTableName($className) . ' where author_id = ' . $authorId, parent::getClassName($className));
+  }
 }
