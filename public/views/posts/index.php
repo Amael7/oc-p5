85c7b1e48d734a6be131cb591b2ad421e22c0b34
stephanemontoro <stephane.montoro@hotmail.com>
2022-01-06 22:31:46 +0100

    front home page

--- public/views/posts/index.php
+++ public/views/posts/index.php
@@ -2,9 +2,11 @@
   <h2>Aucun Articles</h2>
   <?php else: ?>
     <?php foreach($posts as $post): ?>
-      <a href=<?= "/blog/post-" . $post->getId() ?> data-id="<?= $post->getId() ?>" >
-        <h2><?= $post->getTitle() ?></h2>
-      </a>
+      <h2>
+        <a href=<?= "/blog/post-" . $post->getId() ?> data-id="<?= $post->getId() ?>">
+          <?= $post->getTitle() ?>
+        </a>
+      </h2>
       <h3><?= $post->getSubtitle() ?></h3>
       <p><?= $post->getContent() ?></p>
     <?php endforeach; ?>
--- public/views/templates/default.php
+++ public/views/templates/default.php
@@ -8,9 +8,10 @@
     <link href="/public/assets/stylesheets/style.css" rel="stylesheet" /> 
   </head>
   <body>
+    <!-- Header Navbar Start -->
     <div class="header-navbar-container">
       <div class="title-header-nav">
-        <h2>Blog PhP</h2>
+        <h1>Blog PhP</h1>
       </div>
       <div class="navbar-menu-container">
         <ul id="list-navbar-menu">
@@ -29,26 +30,17 @@
         </ul>
       </div> 
     </div>
+    <!-- Header Navbar End -->
 
     <div id="main-container">
       <?= $content ?>
     </div>
 
+    <!-- Footer Navbar Start -->
     <div class="footer-container">
-    <h3>Montoro Stéphane</h3>
-    <div class="social-link-footer">
-      <ul>
-        <li><a a href="https://github.com/Amael7" target="blank"> <i class="fab fa-github"></i></a></li>
-        <li><a href="https://www.linkedin.com/in/stephane-montoro/" target="blank"> <i class="fab fa-linkedin"></i></a></li>
-        <li><a a href="https://twitter.com/MontoroStephane" target="blank"> <i class="fab fa-twitter"></i></a></li>
-      </ul>
-    </div>
-    <?php if (isset($_SESSION['tokenAuth'])): ?>
-      <?php if (isset($admin) && $admin): ?>
-        <li><a href="/admin/dashboard">Dashboard</a></li>
-      <?php endif; ?>
-    <?php endif; ?>
+      <h3>Montoro Stéphane</h3>
       <p>Ce blog est édité par moi-même. Copyright © 2021-2021. Tous droits réservés.</p>
     </div>
+    <!-- Footer Navbar Start -->
   </body>
 </html>
